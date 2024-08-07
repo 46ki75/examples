8f364f0914eadeadc8f7818f30e5645cf5fe0dac
shirayuki <shirayuki@46ki75.com>
2024-08-07 22:45:31 +0900

    test(ts-container): create test code for CDK stack

--- aws/lambda/nodejs/container/test/container.test.ts
+++ aws/lambda/nodejs/container/test/container.test.ts
@@ -1,17 +1,14 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Container from '../lib/container-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { ContainerStack } from "../lib/container-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/container-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Container.ContainerStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test("Lambda Function Created", () => {
+  const app = new cdk.App();
+  const stack = new ContainerStack(app, "MyTestStack");
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties("AWS::Lambda::Function", {
+    FunctionName: "node-container-function",
+    PackageType: "Image",
+  });
 });
