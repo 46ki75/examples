8473ad28a8765be562b80371cdfa8dd53fa0caec
shirayuki <shirayuki@46ki75.com>
2024-08-08 22:46:21 +0900

    docs(lambda-rust-graphql): add description to GraphQL query

--- aws/lambda/rust/lambda-rust-graphql/src/query.rs
+++ aws/lambda/rust/lambda-rust-graphql/src/query.rs
@@ -6,10 +6,12 @@ pub struct GreetQuery;
 
 #[graphql_object]
 impl GreetQuery {
+    #[graphql(description = "Returns a greeting message")]
     fn message(&self) -> &str {
         "Hello, GraphQL!"
     }
 
+    #[graphql(description = "Returns the language of the message")]
     fn language(&self) -> &str {
         "Rust"
     }
@@ -17,6 +19,7 @@ impl GreetQuery {
 
 #[graphql_object]
 impl Query {
+    #[graphql(description = "Returns a GreetQuery object which contains greeting information")]
     fn greet() -> GreetQuery {
         GreetQuery
     }
