d123cb1abb3c2326437e967913732f9444564fdf
shirayuki <shirayuki@46ki75.com>
2024-08-08 18:26:59 +0900

    feat(graphql): implement playground

--- aws/lambda/rust/graphql/src/main.rs
+++ aws/lambda/rust/graphql/src/main.rs
@@ -1,30 +1,24 @@
-use lambda_http::{run, service_fn, tracing, Body, Error, Request, RequestExt, Response};
+use juniper::http::playground::playground_source;
+use lambda_http::{http::Method, run, service_fn, tracing, Body, Error, Request, Response};
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    // Extract some useful information from the request
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
-
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/html")
-        .body(message.into())
-        .map_err(Box::new)?;
-    Ok(resp)
+    if event.method() == Method::GET {
+        let html = playground_source("", None);
+        Ok(Response::builder()
+            .status(200)
+            .header("content-type", "text/html")
+            .body(Body::Text(html))
+            .map_err(Box::new)?)
+    } else {
+        Ok(Response::builder()
+            .status(404)
+            .body(Body::Text("Not Found".to_string()))
+            .map_err(Box::new)?)
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing::init_default_subscriber();
-
     run(service_fn(function_handler)).await
 }
