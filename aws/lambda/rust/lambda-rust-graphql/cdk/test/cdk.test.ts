7c1cb62b219edc12ea8f8b1d3fe6f7ec4fc16ce8
shirayuki <shirayuki@46ki75.com>
2024-08-10 19:08:39 +0900

    test(lambda-rust-graphql): create tests for Lambda stack

--- aws/lambda/rust/lambda-rust-graphql/cdk/test/cdk.test.ts
+++ aws/lambda/rust/lambda-rust-graphql/cdk/test/cdk.test.ts
@@ -1,15 +1,23 @@
-import * as cdk from "aws-cdk-lib";
-import { Template } from "aws-cdk-lib/assertions";
-import { LambdaStack } from "../lib/lambda";
+import * as cdk from 'aws-cdk-lib'
+import { Template } from 'aws-cdk-lib/assertions'
+import { LambdaStack } from '../lib/lambda'
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/cdk-stack.ts
-describe("Lambda Stack Created", () => {
-  test("SQS Queue Created", () => {});
-  const app = new cdk.App();
-  const stack = new LambdaStack(app, "Lambda");
-  const template = Template.fromStack(stack);
-  template.hasResourceProperties("AWS::Lambda::Function", {
-    FunctionName: "rust-graphql-function-url",
-  });
-});
+describe('Lambda Stack Created', () => {
+  const app = new cdk.App()
+  const stack = new LambdaStack(app, 'Lambda')
+  const template = Template.fromStack(stack)
+
+  test('Lambda Funtion', () => {
+    template.hasResourceProperties('AWS::Lambda::Function', {
+      FunctionName: 'rust-graphql-function-url'
+    })
+  })
+
+  test('Lambda Alias', () => {
+    template.hasResourceProperties('AWS::Lambda::Alias', {
+      FunctionVersion: '$LATEST'
+    })
+  })
+})
