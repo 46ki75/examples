e28c62cad2ee9022d2b745611f9d526d9b7a5a91
shirayuki <shirayuki@46ki75.com>
2024-08-10 19:04:40 +0900

    style(lambda-rust-graphql): format code with prettier

--- aws/lambda/rust/lambda-rust-graphql/cdk/bin/cdk.ts
+++ aws/lambda/rust/lambda-rust-graphql/cdk/bin/cdk.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
-import { LambdaStack } from "../lib/lambda";
+import 'source-map-support/register'
+import * as cdk from 'aws-cdk-lib'
+import { LambdaStack } from '../lib/lambda'
 
-const app = new cdk.App();
-new LambdaStack(app, "Lambda", {
+const app = new cdk.App()
+new LambdaStack(app, 'Lambda', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-});
+    region: process.env.CDK_DEFAULT_REGION
+  }
+})
--- aws/lambda/rust/lambda-rust-graphql/cdk/test/cdk.test.ts
+++ aws/lambda/rust/lambda-rust-graphql/cdk/test/cdk.test.ts
@@ -1,17 +1,15 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Cdk from '../lib/cdk-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { LambdaStack } from "../lib/lambda";
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Cdk.CdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+describe("Lambda Stack Created", () => {
+  test("SQS Queue Created", () => {});
+  const app = new cdk.App();
+  const stack = new LambdaStack(app, "Lambda");
+  const template = Template.fromStack(stack);
+  template.hasResourceProperties("AWS::Lambda::Function", {
+    FunctionName: "rust-graphql-function-url",
+  });
 });
