f4f70017ca1b5745611a71fbd57032389cf91903
shirayuki <shirayuki@46ki75.com>
2024-09-01 17:07:22 +0900

    feat(apollo): create basic queries

--- nodejs/apollo/src/index.ts
+++ nodejs/apollo/src/index.ts
@@ -1,1 +1,37 @@
-console.log('Hello, world!')
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+
+const typeDefs = `#graphql
+
+type Greet{
+    message: String!
+    language: String!
+}
+
+type Query {
+    greet: Greet
+}
+`
+
+const resolvers = {
+  Query: {
+    greet: () => ({
+      message: () => 'Hello, GraphQL!',
+      language: async () => {
+        await new Promise((resolve) => setTimeout(resolve, 3000))
+        return 'typescript'
+      }
+    })
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 }
+})
+
+console.log(`🚀  Server ready at: ${url}`)
