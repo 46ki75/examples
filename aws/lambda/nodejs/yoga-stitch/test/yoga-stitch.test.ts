ea99e3585d61db304efa29ac7108ade988f61678
shirayuki <shirayuki@46ki75.com>
2024-09-01 22:47:24 +0900

    feat(yoga-stitch): initialize CDK project

---
+++ aws/lambda/nodejs/yoga-stitch/test/yoga-stitch.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as YogaStitch from '../lib/yoga-stitch-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/yoga-stitch-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new YogaStitch.YogaStitchStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
