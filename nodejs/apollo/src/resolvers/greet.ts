61c73ad80896a282c7f8e315813013b8f060cc74
shirayuki <shirayuki@46ki75.com>
2024-09-01 22:44:43 +0900

    feat(apollo): add project deletion feature

--- nodejs/apollo/src/resolvers/greet.ts
+++
@@ -1,7 +0,0 @@
-export const greet = () => ({
-  message: () => 'Hello, GraphQL!',
-  language: async () => {
-    await new Promise((resolve) => setTimeout(resolve, 3000))
-    return 'typescript'
-  }
-})
--- nodejs/apollo/src/resolvers/index.ts
+++
@@ -1,3 +0,0 @@
-import { greet } from './greet.js'
-
-export const queryResolvers = { greet }
