9b95ea140e634f6833788fab929e904607251990
shirayuki <shirayuki@46ki75.com>
2024-09-01 22:11:09 +0900

    feat(yoga): implement basic query

--- nodejs/yoga/src/index.ts
+++ nodejs/yoga/src/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello, world!')
+import { createSchema } from 'graphql-yoga'
+
+import { createServer } from 'node:http'
+import { createYoga } from 'graphql-yoga'
+import { readFileSync } from 'node:fs'
+
+export const schema = createSchema({
+  typeDefs: readFileSync('./schema.graphql', 'utf-8'),
+  resolvers: {
+    Query: {
+      greet: () => ({
+        message: () => 'Hello, world!',
+        language: () => 'typescript'
+      })
+    }
+  }
+})
+
+const yoga = createYoga({ schema })
+
+const server = createServer(yoga)
+
+server.listen(4000, () => {
+  console.info('Server is running on http://localhost:4000/graphql')
+})
