f205c5642f3b76aa278ccbedcaf0dd38c4061a1b
shirayuki <shirayuki@46ki75.com>
2024-11-15 09:12:07 +0900

    feat: add S3 bucket and Athena workgroup to CommonStack for enhanced data processing

--- packages/cdk-glue-data-catalog/lib/common.ts
+++ packages/cdk-glue-data-catalog/lib/common.ts
@@ -1,12 +1,31 @@
 import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import * as glue from 'aws-cdk-lib/aws-glue'
+import * as athena from 'aws-cdk-lib/aws-athena'
+import * as s3 from 'aws-cdk-lib/aws-s3'
 
 export class CommonStack extends cdk.Stack {
-  database: glue.CfnDatabase
+  private readonly bucket: s3.Bucket
+  private readonly workgroup: athena.CfnWorkGroup
+  readonly database: glue.CfnDatabase
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
+
+    this.bucket = new s3.Bucket(this, 'Bucket', {
+      bucketName: `${this.account}-athena-results`,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    })
+
+    this.workgroup = new athena.CfnWorkGroup(this, 'WorkGroup', {
+      name: 'poc',
+      workGroupConfiguration: {
+        resultConfiguration: {
+          outputLocation: `s3://${this.bucket.bucketName}`
+        }
+      }
+    })
 
     this.database = new glue.CfnDatabase(this, 'Database', {
       databaseInput: { name: 'poc' },
