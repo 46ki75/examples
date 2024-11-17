abb777243fcd7db84c5f28dbf0cee9d02ab8c685
shirayuki <shirayuki@46ki75.com>
2024-11-18 00:54:00 +0900

    feat(terraform): add IAM role and instance profile for SSM access to EC2 instance

--- terraform/aws-ec2/main.tf
+++ terraform/aws-ec2/main.tf
@@ -44,12 +44,44 @@ resource "aws_route_table_association" "route_table_association" {
   route_table_id = aws_route_table.route_table.id
 }
 
+resource "aws_iam_role" "ssm_role" {
+  name = "46ki75-ssm-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "ssm_role_policy" {
+  name       = "46ki75-ssm-role-policy-attachment"
+  roles      = [aws_iam_role.ssm_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm_instance_profile" {
+  name = "46ki75-ssm-instance-profile"
+  role = aws_iam_role.ssm_role.name
+}
+
 resource "aws_instance" "instance" {
   availability_zone           = "ap-northeast-1a"
   instance_type               = "t3.micro"
   ami                         = "ami-094dc5cf74289dfbc"
   subnet_id                   = aws_subnet.subnet.id
   associate_public_ip_address = true
+
+  iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
 
   user_data = <<-EOF
     #!/bin/bash
@@ -61,4 +93,3 @@ resource "aws_instance" "instance" {
     "Name" = "46ki75-aws-ec2-instance"
   }
 }
-
