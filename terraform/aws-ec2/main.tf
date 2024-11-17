bdf17a70ef5a8c041563096529238a29897df23c
shirayuki <shirayuki@46ki75.com>
2024-11-18 00:26:00 +0900

    chore(terraform): move AWS provider configuration to a separate file

--- terraform/aws-ec2/main.tf
+++ terraform/aws-ec2/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.76"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-northeast-1"
-}
-
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "my_bucket" {
