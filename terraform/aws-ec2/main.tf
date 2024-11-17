82b706b6d3566fb9837e71cad2a31c6f897643ff
shirayuki <shirayuki@46ki75.com>
2024-11-18 00:32:30 +0900

    chore(terraform): add VPC and subnet resources to AWS EC2 configuration

--- terraform/aws-ec2/main.tf
+++ terraform/aws-ec2/main.tf
@@ -1,5 +1,24 @@
 data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "${data.aws_caller_identity.current.account_id}-46ki75-com-spec"
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    "Name" = "46ki75-aws-ec2-vpc"
+  }
+}
+
+
+resource "aws_subnet" "subnet" {
+  count = 3
+
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.${count.index + 1}.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    "Name" = "46ki75-aws-ec2-subnet-${count.index + 1}"
+  }
 }
