2e4c91acba2ec9f7b0ea6af418ab92c06e6f7a13
shirayuki <shirayuki@46ki75.com>
2024-11-18 02:20:17 +0900

    feat(terraform): add VPC endpoints for S3, SSM, and EC2 messages with associated security group

---
+++ terraform/aws-ec2-igw/endpoint.tf
@@ -0,0 +1,70 @@
+data "aws_region" "current" {}
+
+resource "aws_security_group" "endpoint" {
+  name   = "46ki75-aws-ec2-sg-endpoint"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    security_groups = [
+      aws_security_group.sg.id,
+    ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.vpc.id
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids = [
+    aws_route_table.route_table.id,
+  ]
+}
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id            = aws_vpc.vpc.id
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.ssm"
+  vpc_endpoint_type = "Interface"
+  subnet_ids = [
+    aws_subnet.subnet.id,
+  ]
+  private_dns_enabled = true
+  security_group_ids = [
+    aws_security_group.endpoint.id,
+  ]
+}
+
+resource "aws_vpc_endpoint" "ssmmessages" {
+  vpc_id            = aws_vpc.vpc.id
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.ssmmessages"
+  vpc_endpoint_type = "Interface"
+  subnet_ids = [
+    aws_subnet.subnet.id,
+  ]
+  private_dns_enabled = true
+  security_group_ids = [
+    aws_security_group.endpoint.id,
+  ]
+}
+
+resource "aws_vpc_endpoint" "ec2messages" {
+  vpc_id            = aws_vpc.vpc.id
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.ec2messages"
+  vpc_endpoint_type = "Interface"
+  subnet_ids = [
+    aws_subnet.subnet.id,
+  ]
+  private_dns_enabled = true
+  security_group_ids = [
+    aws_security_group.endpoint.id,
+  ]
+}
