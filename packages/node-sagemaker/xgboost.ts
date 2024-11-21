9236df67842d3889fc7edd306fd5ed50411e9eb2
shirayuki <shirayuki@46ki75.com>
2024-11-22 01:05:49 +0900

    feat(node-sagemaker): implement training job monitoring and model creation with endpoint configuration

--- packages/node-sagemaker/xgboost.ts
+++ packages/node-sagemaker/xgboost.ts
@@ -2,7 +2,11 @@ import { GetRoleCommand, IAMClient } from '@aws-sdk/client-iam'
 
 import {
   SageMakerClient,
-  CreateTrainingJobCommand
+  CreateTrainingJobCommand,
+  DescribeTrainingJobCommand,
+  CreateModelCommand,
+  CreateEndpointConfigCommand,
+  CreateEndpointCommand
 } from '@aws-sdk/client-sagemaker'
 
 const ROLE_NAME = 'sagemaker-s3-role'
@@ -80,7 +84,46 @@ const createTrainingJobResponse = await sagemakerClient.send(
   createTrainingJobCommand
 )
 
-console.log('Training job created:', createTrainingJobResponse.TrainingJobArn)
+console.log('■ Training job created:', createTrainingJobResponse.TrainingJobArn)
+
+// # --------------------------------------------------------------------------------
+//
+// Wait Training Job
+//
+// # --------------------------------------------------------------------------------
+
+let finished = false
+let modelDataUrl: string | null = null
+
+while (!finished) {
+  const describeTrainingJobCommand = new DescribeTrainingJobCommand({
+    TrainingJobName: createTrainingJobResponse.TrainingJobArn?.split('/').pop()
+  })
+
+  const describeTrainingJobResponse = await sagemakerClient.send(
+    describeTrainingJobCommand
+  )
+
+  console.log(
+    '■ Training job status:',
+    describeTrainingJobResponse.TrainingJobStatus
+  )
+
+  if (
+    describeTrainingJobResponse.TrainingJobStatus === 'Completed' &&
+    describeTrainingJobResponse.ModelArtifacts?.S3ModelArtifacts
+  ) {
+    modelDataUrl = describeTrainingJobResponse.ModelArtifacts.S3ModelArtifacts
+    finished = true
+    console.log('■ Training job completed:', modelDataUrl)
+  } else {
+    await new Promise((resolve) => setTimeout(resolve, 30000))
+  }
+}
+
+if (modelDataUrl == null) {
+  throw new Error('Model data URL not found')
+}
 
 // # --------------------------------------------------------------------------------
 //
@@ -88,16 +131,67 @@ console.log('Training job created:', createTrainingJobResponse.TrainingJobArn)
 //
 // # --------------------------------------------------------------------------------
 
-// const params = {
-//   ModelName: 'MyModel',
-//   PrimaryContainer: {
-//     Image:
-//       '354813040037.dkr.ecr.ap-northeast-1.amazonaws.com/sagemaker-xgboost:1.7-1',
-//     ModelDataUrl: modelS3Uri
-//   },
-//   ExecutionRoleArn: roleArn
-// }
+const modelName = `xgboost-iris-model-${new Date().getTime()}`
 
-// const command = new CreateModelCommand(params)
-// const response = await sagemakerClient.send(command)
-// console.log('Model created:', response.ModelArn)
+const command = new CreateModelCommand({
+  ModelName: modelName,
+  PrimaryContainer: {
+    Image:
+      '354813040037.dkr.ecr.ap-northeast-1.amazonaws.com/sagemaker-xgboost:1.7-1',
+    ModelDataUrl: modelDataUrl
+  },
+  ExecutionRoleArn: roleArn
+})
+
+const response = await sagemakerClient.send(command)
+
+console.log('■ Model created:', response.ModelArn)
+
+// # --------------------------------------------------------------------------------
+//
+// Create Endpoint Config
+//
+// # --------------------------------------------------------------------------------
+
+const endpointConfigName = `xgboost-iris-endpoint-config-${new Date().getTime()}`
+
+const createEndpointConfigCommand = new CreateEndpointConfigCommand({
+  EndpointConfigName: endpointConfigName,
+  ProductionVariants: [
+    {
+      VariantName: 'AllTraffic',
+      ModelName: modelName,
+      ServerlessConfig: {
+        MaxConcurrency: 1,
+        MemorySizeInMB: 1024
+      },
+      InitialVariantWeight: 1.0
+    }
+  ]
+})
+
+const createEndpointConfigResponse = await sagemakerClient.send(
+  createEndpointConfigCommand
+)
+
+console.log(
+  '■ Endpoint config created:',
+  createEndpointConfigResponse.EndpointConfigArn
+)
+
+// # --------------------------------------------------------------------------------
+//
+// Create Endpoint
+//
+// # --------------------------------------------------------------------------------
+
+const endpointName = `xgboost-iris-endpoint-${new Date().getTime()}`
+
+const createEndpointCommand = new CreateEndpointCommand({
+  EndpointName: endpointName,
+  EndpointConfigName: endpointConfigName
+})
+
+const createEndpointResponse = await sagemakerClient.send(createEndpointCommand)
+
+console.log('■ Endpoint created:', createEndpointResponse.EndpointArn)
