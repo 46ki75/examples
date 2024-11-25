07b17e8f15cbe3d586b4306fdd757fb493a32b93
shirayuki <shirayuki@46ki75.com>
2024-11-26 00:35:43 +0900

    feat(terraform): add S3 bucket website configuration for static site hosting

--- terraform/aws-cloudfront-s3/main.tf
+++ terraform/aws-cloudfront-s3/main.tf
@@ -3,9 +3,17 @@ resource "aws_s3_bucket" "static" {
   force_destroy = true
 }
 
-
 resource "aws_s3_object" "file" {
   bucket  = aws_s3_bucket.static.bucket
   key     = "index.html"
   content = file("./index.html")
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/5.70.0/docs/resources/s3_bucket_website_configuration
+resource "aws_s3_bucket_website_configuration" "name" {
+  bucket = aws_s3_bucket.static.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
 }
