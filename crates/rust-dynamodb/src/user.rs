1fed9f2ded1c54a5de26bbc784fe1c96d3571505
shirayuki <shirayuki@46ki75.com>
2024-11-26 23:04:46 +0900

    feat(rust-dynamodb): add get method to User struct and create get_item example

--- crates/rust-dynamodb/src/user.rs
+++ crates/rust-dynamodb/src/user.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 pub struct User {
     /// Partition key
     pub pk: String,
@@ -19,6 +20,73 @@ pub struct User {
 }
 
 impl User {
+    pub async fn get<S, T>(pk: S, sk: T) -> Result<Self, Box<dyn std::error::Error>>
+    where
+        S: AsRef<str>,
+        T: AsRef<str>,
+    {
+        let config = aws_config::load_defaults(aws_config::BehaviorVersion::latest()).await;
+
+        let client = aws_sdk_dynamodb::Client::new(&config);
+
+        let request = client
+            .get_item()
+            .table_name("rust-dynamodb")
+            .key(
+                "PK",
+                aws_sdk_dynamodb::types::AttributeValue::S(pk.as_ref().to_string()),
+            )
+            .key(
+                "SK",
+                aws_sdk_dynamodb::types::AttributeValue::S(sk.as_ref().to_string()),
+            );
+
+        let response = request.send().await?;
+
+        let item = response.item.ok_or("Item not found.")?;
+
+        let user = User {
+            pk: item
+                .get("PK")
+                .ok_or("PK is not found.")?
+                .as_s()
+                .map_err(|e| format!("Failed to convert attribute value to string: {:?}", e))?
+                .to_string(),
+            sk: item
+                .get("SK")
+                .ok_or("SK is not found.")?
+                .as_s()
+                .map_err(|e| format!("Failed to convert attribute value to string: {:?}", e))?
+                .to_string(),
+            id: item
+                .get("PK")
+                .ok_or("PK is not found.")?
+                .as_s()
+                .map_err(|e| format!("Failed to convert attribute value to string: {:?}", e))?
+                .to_string(),
+            name: item
+                .get("name")
+                .ok_or("name is not found.")?
+                .as_s()
+                .map_err(|e| format!("Failed to convert attribute value to string: {:?}", e))?
+                .to_string(),
+            email: item
+                .get("email")
+                .ok_or("email is not found.")?
+                .as_s()
+                .map_err(|e| format!("Failed to convert attribute value to string: {:?}", e))?
+                .to_string(),
+            age: item
+                .get("age")
+                .ok_or("age is not found.")?
+                .as_n()
+                .map_err(|e| format!("Failed to convert attribute value to number: {:?}", e))?
+                .parse()?,
+        };
+
+        Ok(user)
+    }
+
     pub async fn upsert(
         self,
     ) -> Result<aws_sdk_dynamodb::operation::put_item::PutItemOutput, Box<dyn std::error::Error>>
