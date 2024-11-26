ded6e10e417bfd75626112524ac504ce03f2a0a3
shirayuki <shirayuki@46ki75.com>
2024-11-26 22:30:25 +0900

    feat(rust-dynamodb): refactor main function and add create_table example

--- crates/rust-dynamodb/src/bin/greet.rs
+++ crates/rust-dynamodb/src/bin/greet.rs
@@ -1,3 +1,9 @@
-fn main() {
-    print!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let config = aws_config::load_defaults(aws_config::BehaviorVersion::latest()).await;
+    let client = aws_sdk_dynamodb::Client::new(&config);
+    let req = client.list_tables().limit(10);
+    let resp = req.send().await?;
+    println!("Current DynamoDB tables: {:?}", resp.table_names);
+    Ok(())
 }
