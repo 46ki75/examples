a16500260eb1b0511f490b8075ae3f820e644e58
shirayuki <shirayuki@46ki75.com>
2024-11-28 02:59:06 +0900

    feat(rust-lambda-subscription-filter): add event.json and enhance function_handler to log AWS CloudWatch events

--- crates/rust-lambda-subscription-filter/src/main.rs
+++ crates/rust-lambda-subscription-filter/src/main.rs
@@ -1,14 +1,31 @@
-use lambda_runtime::{run, service_fn, tracing, Error, LambdaEvent};
-
 async fn function_handler(
-    event: LambdaEvent<aws_lambda_events::event::cloudwatch_logs::LogsEvent>,
-) -> Result<(), Error> {
-    Ok(())
+    event: lambda_runtime::LambdaEvent<aws_lambda_events::event::cloudwatch_logs::LogsEvent>,
+) -> Result<aws_lambda_events::event::cloudwatch_logs::LogsEvent, lambda_runtime::Error> {
+    println!("■ Log Group: {}", event.payload.aws_logs.data.log_group);
+    println!("■ Log Stream: {}", event.payload.aws_logs.data.log_stream);
+    println!("■ Owner: {}", event.payload.aws_logs.data.owner);
+    println!(
+        "■ Message Type: {}",
+        event.payload.aws_logs.data.message_type
+    );
+    println!(
+        "■ Subscription Filters: {:?}",
+        event.payload.aws_logs.data.subscription_filters
+    );
+
+    for log_event in event.payload.aws_logs.data.log_events.iter() {
+        println!();
+        println!("  □ Log Event ID: {}", log_event.id);
+        println!("  □ Log Event Timestamp: {}", log_event.timestamp);
+        println!("  □ Log Event Message: {}", log_event.message);
+    }
+
+    Ok(event.payload)
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    tracing::init_default_subscriber();
+async fn main() -> Result<(), lambda_runtime::Error> {
+    lambda_runtime::tracing::init_default_subscriber();
 
-    run(service_fn(function_handler)).await
+    lambda_runtime::run(lambda_runtime::service_fn(function_handler)).await
 }
