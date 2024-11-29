dca7c245f93f9f73e91c70df9db0c7ee848c3950
shirayuki <shirayuki@46ki75.com>
2024-11-30 00:09:19 +0900

    feat(node-lambda-graphql): implement new build script with esbuild and archiver for packaging

---
+++ packages/node-lambda-graphql/build.ts
@@ -0,0 +1,39 @@
+import { build } from 'esbuild'
+import { copyFileSync, createWriteStream, mkdirSync, rmdirSync } from 'node:fs'
+import archiver from 'archiver'
+
+try {
+  rmdirSync('./dist', { recursive: true })
+} catch {
+  // ignore
+}
+mkdirSync('./dist/lambda', { recursive: true })
+
+await build({
+  entryPoints: ['./src/index.ts'],
+  outfile: './dist/lambda/index.js',
+  format: 'esm',
+  bundle: true,
+  platform: 'node',
+  target: 'esnext',
+  minify: true,
+  keepNames: true
+})
+
+copyFileSync('./schema.graphql', './dist/lambda/schema.graphql')
+copyFileSync('./graphiql.html', './dist/lambda/graphiql.html')
+
+const output = createWriteStream('./dist/lambda.zip')
+const archive = archiver('zip', { zlib: { level: 9 } })
+
+output.on('close', () => {
+  console.log(
+    `ZIP file created successfully! Total size: ${archive.pointer()} bytes`
+  )
+})
+archive.on('error', (err) => {
+  throw err
+})
+archive.pipe(output)
+archive.directory('./dist/lambda', false)
+await archive.finalize()
