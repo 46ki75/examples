f65e8a5569f65adb171e3097569f4973edd50c51
shirayuki <shirayuki@46ki75.com>
2024-11-29 11:11:50 +0900

    feat(node-lambda-graphql): add GraphQL schema and event handling for Lambda function

--- packages/node-lambda-graphql/src/index.ts
+++ packages/node-lambda-graphql/src/index.ts
@@ -1,8 +1,41 @@
 import { LambdaFunctionURLEvent, Context } from 'aws-lambda'
 
+import { makeExecutableSchema } from '@graphql-tools/schema'
+import { graphql } from 'graphql'
+
+const typeDefs = `
+  type Query {
+    hello: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello from AWS Lambda!'
+  }
+}
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
 export const handler = async (
   event: LambdaFunctionURLEvent,
   context: Context
 ) => {
-  return { event, context }
+  if (event.body == null) {
+    return {
+      error: 'No body provided'
+    }
+  }
+
+  let decodedBody = Buffer.from(event.body, 'base64').toString('utf-8')
+
+  const { query, variables } = JSON.parse(decodedBody)
+
+  const result = await graphql({
+    schema,
+    source: query,
+    variableValues: variables
+  })
+
+  return { result }
 }
