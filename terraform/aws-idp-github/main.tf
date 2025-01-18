493d0b5736afe35eafe4259aade454893b090bda
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-01-19 02:21:29 +0900

    feat: add IAM role and policy for GitHub Actions integration with AWS OIDC provider

--- terraform/aws-idp-github/main.tf
+++ terraform/aws-idp-github/main.tf
@@ -1,9 +1,53 @@
-data "aws_region" "current" {}
-data "aws_caller_identity" "current" {}
-
-
-resource "aws_iam_openid_connect_provider" "name" {
+resource "aws_iam_openid_connect_provider" "main" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = []
+}
+
+resource "aws_iam_role" "main" {
+  name = "shared-46ki75-examples-iam-role-github-actions"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+        },
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : [
+              "sts.amazonaws.com"
+            ]
+          },
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:46ki75/examples:*"
+            ]
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "main" {
+  name        = "shared-46ki75-examples-iam-policy-github-actions"
+  description = "IAM policy for GitHub Actions"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "s3:ListBucket",
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "main" {
+  role       = aws_iam_role.main.name
+  policy_arn = aws_iam_policy.main.arn
 }
