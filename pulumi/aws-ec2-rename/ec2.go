43b2d085de9ed8d7706819b70284d4648555d065
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-01-24 05:17:56 +0900

    feat: add security group configuration to EC2 component

--- pulumi/aws-ec2-rename/ec2.go
+++ pulumi/aws-ec2-rename/ec2.go
@@ -23,9 +23,29 @@ func NewEc2Component(ctx *pulumi.Context, name string, args *Ec2ComponentArgs, o
 		return nil, err
 	}
 
+	sg, err := ec2.NewSecurityGroup(ctx, fmt.Sprintf("%s-46ki75-examples-ec2-security_group-main", ctx.Stack()), &ec2.SecurityGroupArgs{
+		Egress: ec2.SecurityGroupEgressArray{
+			&ec2.SecurityGroupEgressArgs{
+				FromPort: pulumi.Int(0),
+				ToPort:   pulumi.Int(0),
+				Protocol: pulumi.String("-1"),
+				CidrBlocks: pulumi.StringArray{
+					pulumi.String("0.0.0.0/0"),
+				},
+				Ipv6CidrBlocks: pulumi.StringArray{
+					pulumi.String("::/0"),
+				},
+			},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
 	_, err = ec2.NewInstance(ctx, fmt.Sprintf("%s-46ki75-examples-ec2-instance-main", ctx.Stack()), &ec2.InstanceArgs{
 		Ami:                      pulumi.String("ami-094dc5cf74289dfbc"),
 		SubnetId:                 args.SubnetId,
+		SecurityGroups:           pulumi.StringArray{sg.ID()},
 		AvailabilityZone:         pulumi.String("ap-northeast-1a"),
 		InstanceType:             pulumi.String("t3.micro"),
 		AssociatePublicIpAddress: pulumi.Bool(true),
