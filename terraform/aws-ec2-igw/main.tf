1e26e4349232011982b2954e884b2faf1c863473
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-01-23 23:23:36 +0900

    feat: update AWS EC2 Terraform configuration with shared naming conventions and local variables

--- terraform/aws-ec2-igw/main.tf
+++ terraform/aws-ec2-igw/main.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_iam_role" "ssm_role" {
-  name = "46ki75-ssm-role"
+  name = "shared-46ki75-examples-iam-role-ssm"
 
   assume_role_policy = jsonencode({
     "Version" : "2012-10-17",
@@ -18,19 +18,19 @@ resource "aws_iam_role" "ssm_role" {
 }
 
 resource "aws_iam_policy_attachment" "ssm_role_policy" {
-  name       = "46ki75-ssm-role-policy-attachment"
+  name       = "shared-46ki75-examples-iam-policy_attachment-ssm"
   roles      = [aws_iam_role.ssm_role.name]
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_instance_profile" "ssm_instance_profile" {
-  name = "46ki75-ssm-instance-profile"
+  name = "shared-46ki75-examples-iam-instance_profile-ssm"
   role = aws_iam_role.ssm_role.name
 }
 
-resource "aws_security_group" "instance" {
-  name   = "46ki75-aws-ec2-sg"
-  vpc_id = aws_vpc.vpc.id
+resource "aws_security_group" "main" {
+  name   = "shared-46ki75-examples-ec2-security_group-main"
+  vpc_id = aws_vpc.main.id
 
   egress {
     from_port   = 0
@@ -40,20 +40,20 @@ resource "aws_security_group" "instance" {
   }
 }
 
-resource "aws_instance" "instance" {
+resource "aws_instance" "main" {
   availability_zone           = "ap-northeast-1a"
   instance_type               = "t3.micro"
-  ami                         = "ami-094dc5cf74289dfbc"
+  ami                         = local.ami
   subnet_id                   = aws_subnet.subnet.id
   associate_public_ip_address = true
-  security_groups             = [aws_security_group.instance.id]
+  security_groups             = [aws_security_group.main.id]
 
   iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
 
   user_data = file("./cloud-init.yaml")
 
   tags = {
-    "Name" = "46ki75-aws-ec2-instance"
+    "Name" = "shared-46ki75-examples-ec2-instance-main"
   }
 }
 
