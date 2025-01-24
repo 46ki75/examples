ac3fb3217ef0d980b6c55a7f10267a8b9ac86ac1
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-01-24 21:43:23 +0900

    feat: implement EC2 component with security group and instance creation, and add unit tests

--- pulumi/aws-ec2-public/pkg/ec2/ec2.go
+++ pulumi/aws-ec2-public/pkg/ec2/ec2.go
@@ -3,6 +3,7 @@ package ec2
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ec2"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
@@ -10,6 +11,8 @@ import (
 
 type Ec2Component struct {
 	pulumi.ResourceState
+	SecurityGroup *ec2.SecurityGroup
+	Instance      *ec2.Instance
 }
 
 type Ec2ComponentArgs struct {
@@ -26,7 +29,7 @@ func NewEc2Component(ctx *pulumi.Context, name string, args *Ec2ComponentArgs, o
 		return nil, err
 	}
 
-	sg, err := ec2.NewSecurityGroup(ctx, fmt.Sprintf("%s-46ki75-examples-ec2-security_group-main", ctx.Stack()), &ec2.SecurityGroupArgs{
+	component.SecurityGroup, err = ec2.NewSecurityGroup(ctx, fmt.Sprintf("%s-46ki75-examples-ec2-security_group-main", ctx.Stack()), &ec2.SecurityGroupArgs{
 		VpcId: args.VpcId,
 		Egress: ec2.SecurityGroupEgressArray{
 			&ec2.SecurityGroupEgressArgs{
@@ -46,15 +49,17 @@ func NewEc2Component(ctx *pulumi.Context, name string, args *Ec2ComponentArgs, o
 		return nil, err
 	}
 
-	userData, err := os.ReadFile("../assets/cloud-init.yaml")
+	wd, err := os.Getwd()
+	userData := filepath.Join(wd, "../assets/cloud-init.yaml")
+
 	if err != nil {
 		return nil, err
 	}
 
-	_, err = ec2.NewInstance(ctx, fmt.Sprintf("%s-46ki75-examples-ec2-instance-main", ctx.Stack()), &ec2.InstanceArgs{
+	component.Instance, err = ec2.NewInstance(ctx, fmt.Sprintf("%s-46ki75-examples-ec2-instance-main", ctx.Stack()), &ec2.InstanceArgs{
 		Ami:                      pulumi.String("ami-094dc5cf74289dfbc"),
 		SubnetId:                 args.SubnetId,
-		SecurityGroups:           pulumi.StringArray{sg.ID()},
+		SecurityGroups:           pulumi.StringArray{component.SecurityGroup.ID()},
 		AvailabilityZone:         pulumi.String("ap-northeast-1a"),
 		InstanceType:             pulumi.String("t3.micro"),
 		AssociatePublicIpAddress: pulumi.Bool(true),
