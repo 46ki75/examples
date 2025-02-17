5c08c2fc9bc928c4d776ab7dd2a0151d2172afd7
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-02-17 22:07:04 +0900

    feat: add logging functionality with env_logger and log dependencies

--- crates/rust-boilerplate/src/main.rs
+++ crates/rust-boilerplate/src/main.rs
@@ -1,5 +1,11 @@
 mod error;
 
 fn main() {
+    env_logger::init();
+
+    log::info!("This is an info message");
+    log::warn!("This is a warning");
+    log::error!("This is an error");
+
     println!("Hello, world!");
 }
