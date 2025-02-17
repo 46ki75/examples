3f8f8deba382dfc24f02de0f1ed25816c07c9e4d
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-02-17 22:19:14 +0900

    feat: add debug logging to main function

--- crates/rust-boilerplate/src/main.rs
+++ crates/rust-boilerplate/src/main.rs
@@ -3,6 +3,7 @@ mod error;
 fn main() {
     env_logger::init();
 
+    log::debug!("This is a debug message");
     log::info!("This is an info message");
     log::warn!("This is a warning");
     log::error!("This is an error");
