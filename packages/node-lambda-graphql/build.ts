dabc5ceaa6873ea6ffdda27a5aa33140ee81b835
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-02-20 14:03:05 +0000

    feat: improve build script by adding existence check for output directory and enhancing code formatting

--- packages/node-lambda-graphql/build.ts
+++ packages/node-lambda-graphql/build.ts
@@ -1,39 +1,36 @@
-import { build } from 'esbuild'
-import { copyFileSync, createWriteStream, mkdirSync, rmSync } from 'node:fs'
-import archiver from 'archiver'
+import { build } from "esbuild";
+import { copyFileSync, createWriteStream, existsSync, rmSync } from "node:fs";
+import archiver from "archiver";
 
-try {
-  rmSync('./dist', { recursive: true })
-} catch {
-  // ignore
+if (existsSync("./dist")) {
+  rmSync("./dist", { recursive: true });
 }
-mkdirSync('./dist/lambda', { recursive: true })
 
 await build({
-  entryPoints: ['./src/index.ts'],
-  outfile: './dist/lambda/index.js',
-  format: 'esm',
+  entryPoints: ["./src/index.ts"],
+  outfile: "./dist/lambda/index.js",
+  format: "esm",
   bundle: true,
-  platform: 'node',
-  target: 'esnext',
+  platform: "node",
+  target: "esnext",
   minify: true,
-  keepNames: true
-})
+  keepNames: true,
+});
 
-copyFileSync('./schema.graphql', './dist/lambda/schema.graphql')
-copyFileSync('./graphiql.html', './dist/lambda/graphiql.html')
+copyFileSync("./schema.graphql", "./dist/lambda/schema.graphql");
+copyFileSync("./graphiql.html", "./dist/lambda/graphiql.html");
 
-const output = createWriteStream('./dist/lambda.zip')
-const archive = archiver('zip', { zlib: { level: 9 } })
+const output = createWriteStream("./dist/lambda.zip");
+const archive = archiver("zip", { zlib: { level: 9 } });
 
-output.on('close', () => {
+output.on("close", () => {
   console.log(
     `ZIP file created successfully! Total size: ${archive.pointer()} bytes`
-  )
-})
-archive.on('error', (err) => {
-  throw err
-})
-archive.pipe(output)
-archive.directory('./dist/lambda', false)
-await archive.finalize()
+  );
+});
+archive.on("error", (err) => {
+  throw err;
+});
+archive.pipe(output);
+archive.directory("./dist/lambda", false);
+await archive.finalize();
