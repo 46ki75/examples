0aab743e9c3d3f962164f934c8396417af633247
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-02-20 14:51:00 +0000

    feat: refactor HTTP executor to allow dynamic endpoint configuration

--- packages/node-lambda-graphql/src/remote.ts
+++ packages/node-lambda-graphql/src/remote.ts
@@ -4,23 +4,22 @@ import { print } from "graphql";
 
 const HTTP_GRAPHQL_ENDPOINT = "https://countries.trevorblades.com/";
 
-const httpExecutor: AsyncExecutor = async ({
-  document,
-  variables,
-  operationName,
-  extensions,
-}) => {
-  const query = print(document);
-  const fetchResult = await fetch(HTTP_GRAPHQL_ENDPOINT, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({ query, variables, operationName, extensions }),
-  });
-  return fetchResult.json();
-};
+const createHttpExecutor =
+  (endpoint: string): AsyncExecutor =>
+  async ({ document, variables, operationName, extensions }) => {
+    const query = print(document);
+    const fetchResult = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ query, variables, operationName, extensions }),
+    });
+    return fetchResult.json();
+  };
+
+const httpExecutor = createHttpExecutor(HTTP_GRAPHQL_ENDPOINT);
 
 export const remotesSubschema = {
   schema: await schemaFromExecutor(httpExecutor),
