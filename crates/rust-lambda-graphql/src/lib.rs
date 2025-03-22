c79975534302cd62fb799f1ee8261776725d4b1f
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-03-22 05:21:31 +0000

    feat: integrate axum for request handling and update dependencies

--- crates/rust-lambda-graphql/src/lib.rs
+++ crates/rust-lambda-graphql/src/lib.rs
@@ -1,3 +1,5 @@
+use lambda_http::tower::ServiceExt;
+
 pub(crate) mod query;
 
 static SCHEMA: tokio::sync::OnceCell<
@@ -29,64 +31,100 @@ async fn init_schema() -> &'static async_graphql::Schema<
         .await
 }
 
+pub async fn execute_axum(
+    app: axum::Router,
+    event: lambda_http::Request,
+) -> Result<lambda_http::Response<lambda_http::Body>, lambda_http::Error> {
+    let mut axum_request = axum::extract::Request::builder()
+        .method(event.method())
+        .uri(event.uri());
+
+    for (key, value) in event.headers() {
+        axum_request = axum_request.header(key.as_str(), value.as_bytes());
+    }
+
+    let request = axum_request
+        .body(axum::body::Body::from(event.body().to_vec()))
+        .unwrap();
+
+    let axum_response = app.oneshot(request).await?;
+
+    let status = axum_response.status();
+    let body = axum_response.into_body();
+    let body_bytes = axum::body::to_bytes(body, 1024 * 1024).await?;
+
+    let lambda_response = lambda_http::Response::builder()
+        .status(status)
+        .header("content-type", "application/json")
+        .body(lambda_http::Body::Binary(body_bytes.to_vec()))
+        .map_err(Box::new)?;
+
+    Ok(lambda_response)
+}
+
 pub async fn function_handler(
     event: lambda_http::Request,
 ) -> Result<lambda_http::Response<lambda_http::Body>, lambda_http::Error> {
-    let schema = init_schema().await;
+    let app = axum::Router::new().route("/", axum::routing::post("Hello, World!"));
 
-    if event.method() == lambda_http::http::Method::POST {
-        // GraphQL Execution
-        let request_body = event.body();
+    let response = execute_axum(app, event).await?;
 
-        let gql_request = match serde_json::from_slice::<async_graphql::Request>(request_body) {
-            Ok(request) => request,
-            Err(err) => {
-                return Ok(lambda_http::Response::builder()
-                    .status(400)
-                    .header("content-type", "application/json")
-                    .body(
-                        serde_json::json!({"error": format!("Invalid request body: {}", err)})
-                            .to_string()
-                            .into(),
-                    )
-                    .map_err(Box::new)?);
-            }
-        };
+    Ok(response)
+    // let schema = init_schema().await;
 
-        let gql_response = schema.execute(gql_request).await;
+    // if event.method() == lambda_http::http::Method::POST {
+    //     // GraphQL Execution
+    //     let request_body = event.body();
 
-        let response_body = match serde_json::to_string(&gql_response) {
-            Ok(body) => body,
-            Err(err) => {
-                lambda_http::tracing::error!("Failed to serialize response: {}", err);
-                return Ok(lambda_http::Response::builder()
-                    .status(500)
-                    .header("content-type", "application/json")
-                    .body(
-                        serde_json::json!({"error": format!("Failed to serialize response: {}", err)})
-                            .to_string()
-                            .into(),
-                    )
-                    .map_err(Box::new)?);
-            }
-        };
+    //     let gql_request = match serde_json::from_slice::<async_graphql::Request>(request_body) {
+    //         Ok(request) => request,
+    //         Err(err) => {
+    //             return Ok(lambda_http::Response::builder()
+    //                 .status(400)
+    //                 .header("content-type", "application/json")
+    //                 .body(
+    //                     serde_json::json!({"error": format!("Invalid request body: {}", err)})
+    //                         .to_string()
+    //                         .into(),
+    //                 )
+    //                 .map_err(Box::new)?);
+    //         }
+    //     };
 
-        Ok(lambda_http::Response::builder()
-            .status(200)
-            .header("content-type", "application/json")
-            .body(response_body.into())
-            .map_err(Box::new)?)
-    } else {
-        // Error Response - Method Not Allowed
-        let response = lambda_http::Response::builder()
-            .status(405)
-            .header("content-type", "application/json")
-            .body(
-                serde_json::json!({"error":"Method Not Allowed"})
-                    .to_string()
-                    .into(),
-            )
-            .map_err(Box::new)?;
-        Ok(response)
-    }
+    //     let gql_response = schema.execute(gql_request).await;
+
+    //     let response_body = match serde_json::to_string(&gql_response) {
+    //         Ok(body) => body,
+    //         Err(err) => {
+    //             lambda_http::tracing::error!("Failed to serialize response: {}", err);
+    //             return Ok(lambda_http::Response::builder()
+    //                 .status(500)
+    //                 .header("content-type", "application/json")
+    //                 .body(
+    //                     serde_json::json!({"error": format!("Failed to serialize response: {}", err)})
+    //                         .to_string()
+    //                         .into(),
+    //                 )
+    //                 .map_err(Box::new)?);
+    //         }
+    //     };
+
+    //     Ok(lambda_http::Response::builder()
+    //         .status(200)
+    //         .header("content-type", "application/json")
+    //         .body(response_body.into())
+    //         .map_err(Box::new)?)
+    // } else {
+    //     // Error Response - Method Not Allowed
+    //     let response = lambda_http::Response::builder()
+    //         .status(405)
+    //         .header("content-type", "application/json")
+    //         .body(
+    //             serde_json::json!({"error":"Method Not Allowed"})
+    //                 .to_string()
+    //                 .into(),
+    //         )
+    //         .map_err(Box::new)?;
+    //     Ok(response)
+    // }
 }
