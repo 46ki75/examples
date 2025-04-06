014ba6f2aadadcf76949bbedc9d33220293618fc
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-04-06 02:25:25 +0000

    feat: add parseQueryParams function and update handler to use it

--- packages/node-lambda-template/src/index.ts
+++ packages/node-lambda-template/src/index.ts
@@ -3,14 +3,17 @@ import type {
   Context,
   LambdaFunctionURLResult,
 } from "aws-lambda";
+import { parseQueryParams as parseQueryParams } from "./lib.js";
 
 export const handler = async (
   event: LambdaFunctionURLEvent,
   context: Context
 ): Promise<LambdaFunctionURLResult> => {
+  const { name } = await parseQueryParams(event.queryStringParameters);
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello, Lambda!", event }),
     headers: { "Contetn-Type": "application/json" },
+    body: JSON.stringify({ message: `Hello, ${name}!` }),
   };
 };
---
+++ packages/node-lambda-template/tests/it/index.test.ts
@@ -0,0 +1,6 @@
+import test from "node:test";
+import assert from "node:assert";
+
+test("1 + 1 は 2", () => {
+  assert.strictEqual(1 + 1, 2);
+});
