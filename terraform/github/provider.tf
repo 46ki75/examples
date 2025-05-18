f2786784efae8f29d8d9314cae5c34098a4ddea0
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-05-18 02:38:39 +0000

    chore: remove outdated comments regarding non-default workspace usage in Terraform configuration

--- terraform/github/provider.tf
+++ terraform/github/provider.tf
@@ -6,11 +6,6 @@ terraform {
     }
   }
 
-  # When using a non-default workspace, the state path will be `<workspace_key_prefix>/<workspace_name>/<key>`
-  # @see https://developer.hashicorp.com/terraform/language/backend/s3#key
-  # RUN: terraform workspace show
-  # RUN: terraform workspace list
-  # RUN: terraform workspace select dev
   backend "s3" {
     bucket       = "shared-46ki75-examples-s3-bucket-terraform-tfstate"
     key          = "github/terraform.tfstate"
