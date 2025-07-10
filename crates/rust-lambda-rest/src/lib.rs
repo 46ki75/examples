0e9c083c5e6b1e13123f843304fb75a20ff46a5d
Ikuma Yamashita <me@ikuma.cloud>
2025-07-10 11:59:00 +0000

    refactor: simplify function_handler by removing dispatch_request

--- crates/rust-lambda-rest/src/lib.rs
+++ crates/rust-lambda-rest/src/lib.rs
@@ -1,31 +1,14 @@
 pub mod axum_handler;
 pub mod router;
 
-async fn dispatch_request(
-    app: axum::Router,
-    event: lambda_http::Request,
-) -> Result<lambda_http::Response<lambda_http::Body>, lambda_http::Error> {
-    use lambda_http::tower::ServiceExt;
-
-    let axum_response = app.oneshot(event).await?;
-
-    let (axum_parts, axum_body) = axum_response.into_parts();
-
-    let axum_body_bytes = axum::body::to_bytes(axum_body, usize::MAX).await?;
-
-    let lambda_body = lambda_http::Body::Binary(axum_body_bytes.into());
-
-    let lambda_response = lambda_http::Response::from_parts(axum_parts, lambda_body);
-
-    Ok(lambda_response)
-}
-
 pub async fn function_handler(
     event: lambda_http::Request,
-) -> Result<lambda_http::Response<lambda_http::Body>, lambda_http::Error> {
+) -> Result<axum::response::Response<axum::body::Body>, lambda_http::Error> {
     let router = crate::router::init_router();
 
-    let response = dispatch_request(router, event).await?;
+    use lambda_http::tower::ServiceExt;
 
-    Ok(response)
+    let axum_response = router.oneshot(event).await?;
+
+    Ok(axum_response)
 }
