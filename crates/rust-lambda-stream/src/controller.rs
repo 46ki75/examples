e0f4da44fb7f64686a949b220a1519df0697a4ce
Ikuma Yamashita <me@ikuma.cloud>
2025-08-04 13:05:56 +0000

    feat: add AWS Bedrock runtime integration and new chat endpoint

--- crates/rust-lambda-stream/src/controller.rs
+++ crates/rust-lambda-stream/src/controller.rs
@@ -1,3 +1,5 @@
+use aws_sdk_bedrockruntime::primitives::Blob;
+
 pub async fn greet_stream(
 ) -> Result<axum::response::Response<axum::body::Body>, axum::http::StatusCode> {
     use futures::stream::StreamExt;
@@ -32,6 +34,69 @@ pub async fn greet() -> Result<axum::response::Response<axum::body::Body>, axum:
     let resp = lambda_http::Response::builder()
         .status(200)
         .header("content-type", "text/plain")
+        .body(body)
+        .map_err(Box::new)
+        .unwrap();
+
+    Ok(resp)
+}
+
+pub async fn invoke() -> Result<axum::response::Response<axum::body::Body>, axum::http::StatusCode>
+{
+    let sdk_config = aws_config::load_defaults(aws_config::BehaviorVersion::latest()).await;
+    let client = aws_sdk_bedrockruntime::Client::new(&sdk_config);
+
+    let receiver = client
+        .invoke_model_with_response_stream()
+        .model_id("apac.anthropic.claude-sonnet-4-20250514-v1:0")
+        .body(Blob::from(
+            serde_json::json!({
+            "messages": [{ 
+                "role": "user", 
+                "content": [{ "type": "text", "text": "Can you tell me about the advantages of Amazon Bedrock?" }]
+            }],
+            "anthropic_version": "bedrock-2023-05-31",
+            "max_tokens": 1000
+        })
+                .to_string()
+                .as_bytes(),
+        ))
+        .send()
+        .await
+        .unwrap()
+        .body;
+
+    let stream = futures::stream::try_unfold((receiver, 0), |(mut receiver, mut id)| async move {
+        type Receiver = aws_sdk_bedrockruntime::primitives::event_stream::EventReceiver<
+            aws_sdk_bedrockruntime::types::ResponseStream,
+            aws_sdk_bedrockruntime::types::error::ResponseStreamError,
+        >;
+
+        let recieved = receiver.recv().await;
+
+        Ok::<Option<(String, (Receiver, u32))>, std::convert::Infallible>(match recieved {
+            Ok(Some(event)) => {
+                let payload_part = event.as_chunk().unwrap();
+
+                if let Some(blob) = payload_part.bytes() {
+                    let data = String::from_utf8_lossy(blob.as_ref()).to_string();
+                    id += 1;
+                    let event = format!("id: {id}\nevent: message\ndata: {data}\n\n");
+                    Some((event, (receiver, id)))
+                } else {
+                    None
+                }
+            }
+            Ok(None) => None,
+            Err(_) => None,
+        })
+    });
+
+    let body = axum::body::Body::from_stream(stream);
+
+    let resp = lambda_http::Response::builder()
+        .status(200)
+        .header("content-type", "text/event-stream")
         .body(body)
         .map_err(Box::new)
         .unwrap();
