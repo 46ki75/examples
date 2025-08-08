8c95422b556aee31f3c58abe854d09fe4237f23f
Ikuma Yamashita <me@ikuma.cloud>
2025-08-08 14:20:16 +0000

    Remove Java and Kotlin AWS Lambda projects and add a new Spring Boot project for AWS Lambda integration

---
+++ java/aws/lambda-spring-web/src/main/java/cloud/ikuma/lambda_spring_web/LambdaSpringWebApplication.java
@@ -0,0 +1,13 @@
+package cloud.ikuma.lambda_spring_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdaSpringWebApplication.class, args);
+	}
+
+}
---
+++ java/aws/lambda-spring-web/src/test/java/cloud/ikuma/lambda_spring_web/LambdaSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.ikuma.lambda_spring_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaSpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
