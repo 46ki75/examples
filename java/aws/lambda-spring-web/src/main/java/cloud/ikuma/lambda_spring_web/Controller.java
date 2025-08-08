e1c1e81553b30a4332ca4ed4c62443f115d5709a
Ikuma Yamashita <me@ikuma.cloud>
2025-08-08 14:31:29 +0000

    Add Controller class with a simple hello endpoint

---
+++ java/aws/lambda-spring-web/src/main/java/cloud/ikuma/lambda_spring_web/Controller.java
@@ -0,0 +1,16 @@
+package cloud.ikuma.lambda_spring_web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public ResponseEntity<String> hello() {
+        ResponseEntity<String> response = ResponseEntity.ok().header("Content-Type", "text/plain")
+                .body("Hello, World!");
+        return response;
+    }
+}
