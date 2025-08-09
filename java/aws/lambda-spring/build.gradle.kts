be8f88ee20f4e6c7e884cb25c278363ea842a960
Ikuma Yamashita <me@ikuma.cloud>
2025-08-09 01:26:04 +0000

    feat: add AWS Lambda support with Spring Cloud Function

--- java/aws/lambda-spring/build.gradle.kts
+++ java/aws/lambda-spring/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.5.4"
 	id("io.spring.dependency-management") version "1.1.7"
+    id("com.github.johnrengelman.shadow") version "8.1.1" 
 }
 
 group = "cloud.ikuma"
@@ -19,10 +20,29 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.cloud:spring-cloud-function-adapter-aws:4.3.0")
+	implementation("org.springframework.cloud:spring-cloud-function-context:4.3.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks {
+    shadowJar {
+        // For AWS Lambda
+        archiveFileName.set("lambda-spring.jar")
+		mergeServiceFiles()
+
+		mergeServiceFiles {
+			include("META-INF/spring/org.springframework.boot.autoconfigure.AutoConfiguration.imports")
+			include("META-INF/spring.factories")
+		}
+
+		manifest {
+			attributes("Start-Class" to "cloud.ikuma.lambda_spring.LambdaSpringApplication")
+		}
+    }
 }
