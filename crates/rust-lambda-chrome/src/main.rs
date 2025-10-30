ddb3bd7ca9920dcc53e02919b105da51bdacbed0
Ikuma Yamashita <me@ikuma.cloud>
2025-10-30 22:13:01 +0000

    feat: initialize rust-lambda-chrome with cargo-lambda

--- crates/rust-lambda-chrome/src/main.rs
+++ crates/rust-lambda-chrome/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use lambda_runtime::{run, service_fn, tracing, Error};
+mod generic_handler;
+use generic_handler::function_handler;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing::init_default_subscriber();
+
+    run(service_fn(function_handler)).await
 }
