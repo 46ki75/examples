d8e93fd0c1937a354e7ee0fbe4ba2a7bb31804ef
Ikuma Yamashita <me@ikuma.cloud>
2025-11-14 23:54:55 +0900

    chore: rename directory to aws-cloudfront

---
+++ terraform/aws-cloudfront/provider.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.7"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.4"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "us-east-1"
+}
+
+# data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
