141db82476e6dca433e7a120c65b165b1ab82859
Ikuma Yamashita <me@ikuma.cloud>
2025-12-16 16:19:52 +0000

    feat: add S3 bucket policy and SNS topic policy for AWS Config

--- packages/cdk/lib/config/index.ts
+++ packages/cdk/lib/config/index.ts
@@ -21,6 +21,7 @@ export class ConfigStack extends Stack {
 
     const prefix = `${DEPLOY_ENV}-${ORG_NAME}-${PROJECT_NAME}`;
 
+    // S3 Bucket for AWS Config
     const configBucket = new aws_s3.Bucket(this, "ConfigBucket", {
       bucketName: `${prefix}-config`,
       encryption: aws_s3.BucketEncryption.S3_MANAGED,
@@ -36,25 +37,100 @@ export class ConfigStack extends Stack {
       autoDeleteObjects: true,
     });
 
-    const primaryTopicName = `${prefix}-SNS-Topic-primary`;
-    const primaryTopic = new aws_sns.Topic(this, "PrimaryTopic", {
-      topicName: primaryTopicName,
-    });
-
-    const primaryDeliveryChannelName = `${prefix}-Config-DeliveryChannel-primary`;
-    const primaryDeliveryChannel = new aws_config.CfnDeliveryChannel(
+    // S3 Bucket Policy using CfnBucketPolicy
+    // https://docs.aws.amazon.com/config/latest/developerguide/s3-bucket-policy.html
+    const configBucketPolicy = new aws_s3.CfnBucketPolicy(
       this,
-      primaryDeliveryChannelName,
+      "ConfigBucketPolicy",
       {
-        name: primaryDeliveryChannelName,
-        s3BucketName: configBucket.bucketName,
-        snsTopicArn: primaryTopic.topicArn,
-        configSnapshotDeliveryProperties: {
-          deliveryFrequency: "TwentyFour_Hours",
+        bucket: configBucket.bucketName,
+        policyDocument: {
+          Version: "2012-10-17",
+          Statement: [
+            {
+              Sid: "AWSConfigBucketPermissionsCheck",
+              Effect: "Allow",
+              Principal: {
+                Service: "config.amazonaws.com",
+              },
+              Action: "s3:GetBucketAcl",
+              Resource: configBucket.bucketArn,
+              Condition: {
+                StringEquals: {
+                  "AWS:SourceAccount": Aws.ACCOUNT_ID,
+                },
+              },
+            },
+            {
+              Sid: "AWSConfigBucketExistenceCheck",
+              Effect: "Allow",
+              Principal: {
+                Service: "config.amazonaws.com",
+              },
+              Action: "s3:ListBucket",
+              Resource: configBucket.bucketArn,
+              Condition: {
+                StringEquals: {
+                  "AWS:SourceAccount": Aws.ACCOUNT_ID,
+                },
+              },
+            },
+            {
+              Sid: "AWSConfigBucketDelivery",
+              Effect: "Allow",
+              Principal: {
+                Service: "config.amazonaws.com",
+              },
+              Action: "s3:PutObject",
+              Resource: `${configBucket.bucketArn}/AWSLogs/${Aws.ACCOUNT_ID}/Config/*`,
+              Condition: {
+                StringEquals: {
+                  "s3:x-amz-acl": "bucket-owner-full-control",
+                  "AWS:SourceAccount": Aws.ACCOUNT_ID,
+                },
+              },
+            },
+          ],
         },
       }
     );
 
+    // SNS Topic for AWS Config notifications
+    const primaryTopicName = `${prefix}-SNS-Topic-primary`;
+    const primaryTopic = new aws_sns.CfnTopic(this, "PrimaryTopic", {
+      topicName: primaryTopicName,
+      // Note: Do NOT enable KmsMasterKeyId - AWS Config doesn't support encrypted SNS topics
+    });
+
+    // SNS Topic Policy using CfnTopicPolicy
+    const primaryTopicPolicy = new aws_sns.CfnTopicPolicy(
+      this,
+      "PrimaryTopicPolicy",
+      {
+        topics: [primaryTopic.ref],
+        policyDocument: {
+          Version: "2012-10-17",
+          Statement: [
+            {
+              Sid: "AWSConfigSNSPolicy",
+              Effect: "Allow",
+              Principal: {
+                Service: "config.amazonaws.com",
+              },
+              Action: "SNS:Publish",
+              Resource: primaryTopic.ref,
+              Condition: {
+                StringEquals: {
+                  "AWS:SourceAccount": Aws.ACCOUNT_ID,
+                },
+              },
+            },
+          ],
+        },
+      }
+    );
+
+    // Service-Linked Role for AWS Config
     const awsConfigServiceLinkedRole = new aws_iam.CfnServiceLinkedRole(
       this,
       "AWSServiceRoleForConfig",
@@ -63,6 +139,22 @@ export class ConfigStack extends Stack {
       }
     );
 
+    // Delivery Channel
+    const primaryDeliveryChannelName = `${prefix}-Config-DeliveryChannel-primary`;
+    const primaryDeliveryChannel = new aws_config.CfnDeliveryChannel(
+      this,
+      primaryDeliveryChannelName,
+      {
+        name: primaryDeliveryChannelName,
+        s3BucketName: configBucket.bucketName,
+        snsTopicArn: primaryTopic.attrTopicArn,
+        configSnapshotDeliveryProperties: {
+          deliveryFrequency: "TwentyFour_Hours",
+        },
+      }
+    );
+
+    // Configuration Recorder
     const primaryConfigRecorderName = `${prefix}-Config-ConfigurationRecorder-primary`;
     const primaryConfigRecorder = new aws_config.CfnConfigurationRecorder(
       this,
