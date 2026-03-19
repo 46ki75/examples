b6838560abc7049f11129c3c12a4a1a703dc1214
Ikuma Yamashita <me@ikuma.cloud>
2026-03-19 20:57:18 +0900

    feat: add callback approval and rejection scripts for durable function

---
+++ packages/node-lambda-durable-function/scripts/callback-reject.ts
@@ -0,0 +1,24 @@
+import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
+import {
+  LambdaClient,
+  SendDurableExecutionCallbackFailureCommand,
+} from "@aws-sdk/client-lambda";
+
+const ssmClient = new SSMClient({});
+
+const callbackIdParameter = await ssmClient.send(
+  new GetParameterCommand({
+    Name: `/node-lambda-durable-function/callback-id`,
+  }),
+);
+
+const callbackId = callbackIdParameter.Parameter?.Value;
+
+const lambdaClient = new LambdaClient({});
+
+await lambdaClient.send(
+  new SendDurableExecutionCallbackFailureCommand({
+    CallbackId: callbackId,
+    Error: { ErrorData: JSON.stringify({ approved: false }) },
+  }),
+);
